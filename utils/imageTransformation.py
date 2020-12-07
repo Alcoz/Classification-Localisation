069caf58182a20e250113f14e3a425ffdf3839b1
Alcoz <baptiste-darnala@hotmail.fr>
2020-12-07 11:27:11 +0100

    Efficient learning for classification task, problem for localisation task

--- utils/imageTransformation.py
+++ utils/imageTransformation.py
@@ -1,5 +1,5 @@
 from skimage import transform
-from skimage.util import img_as_float
+from skimage.util import img_as_float32, img_as_ubyte
 import cv2
 import torch
 import numpy as np
@@ -10,7 +10,7 @@ class Rescale(object):
         self.output_size = output_size
 
     def __call__(self, sample):
-        image, bndbox, label = img_as_float(sample['image']), sample['bndbox'], sample['label']
+        image, bndbox, label = img_as_float32(img_as_ubyte(sample['image'])), sample['bndbox'], sample['label']
 
         h, w = image.shape[:2]
         new_h, new_w = self.output_size
@@ -18,7 +18,6 @@ class Rescale(object):
         new_h, new_w = int(new_h), int(new_w)
 
         img = cv2.resize(image, (new_h, new_w))
-
         bndbox = np.array([int(bndbox[0] * new_w / w), int(bndbox[1] * new_h / h), int(bndbox[2] * new_w / w), int(bndbox[3] * new_h / h)])
 
         return {'image': img, 'bndbox': bndbox, 'label': label}
@@ -34,6 +33,7 @@ class ToTensor(object):
         # numpy image: H x W x C
         # torch image: C X H X W
         image = image.transpose((2, 0, 1))
+
         return {'image': torch.from_numpy(image),
                 'bndbox': torch.from_numpy(bndbox),
                 'label': label}
@@ -43,7 +43,9 @@ class Normalize(object):
         image, bndbox, label = sample['image'], sample['bndbox'], sample['label']
 
         norm = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        image = norm(image.float())
+
+        image = norm(image)
+
         return {'image': image,
                 'bndbox': bndbox,
                 'label': label}
